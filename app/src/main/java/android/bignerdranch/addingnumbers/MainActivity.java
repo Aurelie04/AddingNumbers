be60f8edc2b6d2855773df2f33469af866ff270c
Aurelie04 <aurelienana25@gmail.com>
2022-03-17 19:45:44 +0200

    To add numbers

---
+++ app/src/main/java/android/bignerdranch/addingnumbers/MainActivity.java
@@ -0,0 +1,39 @@
+package android.bignerdranch.addingnumbers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText number1;
+    private EditText number2;
+    private Button add;
+    private TextView answer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1 = (EditText) findViewById(R.id.edTextNumber1);
+        number2 = (EditText) findViewById(R.id.edTextNumber2);
+        add = (Button) findViewById(R.id.btnAdd);
+        answer = (TextView) findViewById(R.id.Answer);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1 = Integer.parseInt(number1.getText().toString());
+                int num2 = Integer.parseInt(number2.getText().toString());
+                int sum = num1 + num2;
+                answer.setText("The Answer is " + String.valueOf(sum));
+
+            }
+        });
+    }
+}
